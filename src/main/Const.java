4fb4b4d22e2c5bdad063dde1e05457f0147370f7
ruy16109 <ruijie.yu@uconn.edu>
2019-04-07 20:34:34 -0400

    attempting to maximize window

--- src/main/Const.java
+++ src/main/Const.java
@@ -1,11 +1,14 @@
 package main;
 
+import java.awt.GraphicsEnvironment;
 import java.io.PrintStream;
 
 public final class Const {
-    private static final int EQ = 1500;
+    private static final int offset = 32;
+    private static final int EQ = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height
+            - offset;
     public static final int WIDTH = EQ, HEIGHT = EQ;
-    public static final int SWAPS = 100, COMPARES = 600;
-    
+    public static final int SWAPS = 10, COMPARES = 60;
+
     public static PrintStream out, err;
 }
